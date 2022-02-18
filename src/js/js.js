8e6751619ff8d6c9973f596abf0d087637f5fc9b
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-02-18 21:38:20 +0200

    1

---
+++ src/js/js.js
@@ -0,0 +1,11 @@
+import './css/styles.css';
+
+// fetch(`https://restcountries.com/v3.1/all`)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(country => {
+//     console.log(country);
+//   });
+
+console.log(5);
